9fdebc76be551bdead902f42452bd6770ad1cfa1
lfalcaolopes <lfalcaolopes@gmail.com>
2023-01-19 17:58:09 -0300

    Day 2

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
-import './styles/global.css';
+import "./styles/global.css";
+import { Header } from "./components/header";
+import { SummaryTable } from "./components/SummaryTable";
 
-function App() {
-
+export function App() {
   return (
-    <p>hello world</p>
-  )
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
+  );
 }
-
-export default App
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx", "./index.html"],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#09090a",
+      },
+      fontFamily: {
+        regular: "Inter_400Regular",
+        semibold: "Inter_600SemiBold",
+        bold: "Inter_700Bold",
+        extrabold: "Inter_800ExtraBold",
+      },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
